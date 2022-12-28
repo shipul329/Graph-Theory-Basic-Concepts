0ae853e6bce7a8fc802a60f8c6845a8cb44c428d
Shipul Roy <59229356+shipul329@users.noreply.github.com>
2022-12-29 02:45:31 +0600

    Update cycle_in_directed_graph.cpp

--- cycle_in_directed_graph.cpp
+++ cycle_in_directed_graph.cpp
@@ -78,6 +78,96 @@ int32_t main()
     return 0;
 }
 
+
+/* ................................................ 2nd approach..................................... */
+
+/// find cycle in directed graph
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define for0(i, n) for(int i=0; i<n; i++)
+#define for1(i, n) for(int i=1; i<=n; i++)
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define nn '\n'
+
+bool iscycle_util(vector<int>adj[], vector<bool>vis, int cur)
+{
+    if(vis[cur]==true)
+    return true;
+    
+    vis[cur]=true;
+    
+    for(int i=0; i<adj[cur].size(); i++)
+    {
+        if(iscycle_util(adj, vis, adj[cur][i]))
+        return true;
+    }
+    
+    return false;
+}
+
+bool iscycle(int n, vector<int>adj[])
+{
+    vector<bool>vis(n, false);
+    
+    for0(i, n)
+    {
+        vis[i]=true;
+        for0(j, adj[i].size())
+        {
+            if(iscycle_util(adj, vis, adj[i][j]))
+            return true;
+        }
+        
+        vis[i]=false;
+    }
+    
+    return false;
+}
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    
+    vector<int>adj[n];
+    
+    for0(i, m)
+    {
+        int u, v;
+        cin >> u >> v;
+        
+        adj[u].pb(v);
+    }
+    
+    if(iscycle(n, adj))
+    cout << "YES!" << nn;
+    
+    else cout << "NO!" << nn;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin >> t;
+    
+    while(t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
+
 /*
 intput 
 1
